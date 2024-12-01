395349acac59fae2b099754d4f61475d28816144
chirayu86 <56358742+chirayu86@users.noreply.github.com>
2024-12-01 18:10:09 +0530

    Create kthElement.cpp

---
+++ SortAndSEARCH/kthElement.cpp
@@ -0,0 +1,30 @@
+int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+        vector<int> merged;
+        int i=0;
+        int j=0;
+        
+        
+        while(i<arr1.size()&&j<arr2.size()) {
+            
+            if(arr1[i]<=arr2[j]) {
+                merged.push_back(arr1[i]);
+                i++;
+            } else {
+                merged.push_back(arr2[j]);
+                j++;
+            }
+        }
+        
+        while(i<arr1.size()) {
+            merged.push_back(arr1[i]);
+            i++;
+        }
+        
+        while(j<arr2.size()) {
+            merged.push_back(arr2[j]);
+        }
+        
+        
+        return merged[k];
+        
+    }
